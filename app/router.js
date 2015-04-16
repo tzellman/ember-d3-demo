80fc58d10052195b4f058d0fc29f0818a1d50678
Tom Zellman <tzellman@gmail.com>
2015-04-16 16:35:34 -0400

    initial commit

---
+++ app/router.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+    location: config.locationType
+});
+
+export default Router.map(function () {
+    this.route('demo1');
+    this.route('demo2');
+});
---
+++ app/routes/demo1.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+    model: function () {
+        return {
+            data: [
+                {label: "1", value: 10},
+                {label: "2", value: 20},
+                {label: "3", value: 15},
+                {label: "4", value: 5},
+                {label: "5", value: 5},
+                {label: "5", value: 5},
+                {label: "5", value: 5},
+                {label: "5", value: 5},
+                {label: "5", value: 5}
+            ]
+        };
+    }
+});
